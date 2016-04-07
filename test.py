1aa527f044fd4252decfbdaa6442ad2adb04055c
Daniel Chudnov <dchud@umich.edu>
2016-04-07 18:19:12 -0400

    added some edgelists, switched most files from json to csv

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ test.py - initial attempt at automating dn flows using luigi
 """
 
 from collections import Counter
+import csv
 import hashlib
 import json
 import logging
@@ -15,7 +16,9 @@ import luigi
 import requests
 import twarc
 
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.WARN)
+
+media_files = []
 
 
 def time_hash(digits=6):
@@ -53,10 +56,10 @@ class FetchTweets(luigi.Task, Twarcy):
     name = luigi.Parameter()
     term = luigi.Parameter()
     lang = luigi.Parameter(default='en')
-    count = luigi.IntParameter(default=1000)
+    count = luigi.IntParameter(default=2000)
 
     def output(self):
-        fname = 'data/%s-tweets.json' % self.name
+        fname = 'data/%s/tweets.json' % self.name
         return luigi.LocalTarget(fname)
 
     def run(self):
@@ -81,18 +84,47 @@ class CountHashtags(luigi.Task):
         return FetchTweets(name=self.name, term=self.term)
 
     def output(self):
-        fname = self.input().fn.replace('.json', '-hashtags.json')
+        fname = self.input().fn.replace('tweets.json', 'count-hashtags.csv')
         return luigi.LocalTarget(fname)
 
     def run(self):
         c = Counter()
-        fp_counts = self.output().open('w')
         for tweet_str in self.input().open('r'):
             tweet = json.loads(tweet_str)
             c.update([ht['text'].lower()
                       for ht in tweet['entities']['hashtags']])
-        json.dump(c, fp_counts, sort_keys=True, indent=2)
-        fp_counts.close()
+        with self.output().open('w') as fp_counts:
+            writer = csv.DictWriter(fp_counts, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['hashtag', 'count'])
+            writer.writeheader()
+            for ht, count in c.items():
+                writer.writerow({'hashtag': ht, 'count': count})
+
+
+class EdgelistHashtags(luigi.Task):
+    name = luigi.Parameter()
+    term = luigi.Parameter()
+
+    def requires(self):
+        return FetchTweets(name=self.name, term=self.term)
+
+    def output(self):
+        fname = self.input().fn.replace('tweets.json', 'edgelist-hashtags.csv')
+        return luigi.LocalTarget(fname)
+
+    def run(self):
+        """Each edge is a tuple containing (screen_name, mentioned_hashtag)"""
+        with self.output().open('w') as fp_csv:
+            writer = csv.DictWriter(fp_csv, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['user', 'hashtag'])
+            writer.writeheader()
+            for tweet_str in self.input().open('r'):
+                tweet = json.loads(tweet_str)
+                for ht in tweet['entities']['hashtags']:
+                    writer.writerow({'user': tweet['user']['screen_name'],
+                                     'hashtag': ht['text'].lower()})
 
 
 class CountUrls(luigi.Task):
@@ -103,18 +135,22 @@ class CountUrls(luigi.Task):
         return FetchTweets(name=self.name, term=self.term)
 
     def output(self):
-        fname = self.input().fn.replace('.json', '-urls.json')
+        fname = self.input().fn.replace('tweets.json', 'count-urls.csv')
         return luigi.LocalTarget(fname)
 
     def run(self):
         c = Counter()
-        fp_counts = self.output().open('w')
         for tweet_str in self.input().open('r'):
             tweet = json.loads(tweet_str)
             c.update([url['expanded_url'].lower()
                       for url in tweet['entities']['urls']])
-        json.dump(c, fp_counts, sort_keys=True, indent=2)
-        fp_counts.close()
+        with self.output().open('w') as fp_counts:
+            writer = csv.DictWriter(fp_counts, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['url', 'count'])
+            writer.writeheader()
+            for url, count in c.items():
+                writer.writerow({'url': url, 'count': count})
 
 
 class CountDomains(luigi.Task):
@@ -125,18 +161,22 @@ class CountDomains(luigi.Task):
         return FetchTweets(name=self.name, term=self.term)
 
     def output(self):
-        fname = self.input().fn.replace('.json', '-domains.json')
+        fname = self.input().fn.replace('tweets.json', 'count-domains.csv')
         return luigi.LocalTarget(fname)
 
     def run(self):
         c = Counter()
-        fp_counts = self.output().open('w')
         for tweet_str in self.input().open('r'):
             tweet = json.loads(tweet_str)
             c.update([urlparse(url['expanded_url']).netloc.lower()
                       for url in tweet['entities']['urls']])
-        json.dump(c, fp_counts, sort_keys=True, indent=2)
-        fp_counts.close()
+        with self.output().open('w') as fp_counts:
+            writer = csv.DictWriter(fp_counts, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['url', 'count'])
+            writer.writeheader()
+            for url, count in c.items():
+                writer.writerow({'url': url, 'count': count})
 
 
 class CountMentions(luigi.Task):
@@ -147,18 +187,48 @@ class CountMentions(luigi.Task):
         return FetchTweets(name=self.name, term=self.term)
 
     def output(self):
-        fname = self.input().fn.replace('.json', '-mentions.json')
+        fname = self.input().fn.replace('tweets.json', 'count-mentions.csv')
         return luigi.LocalTarget(fname)
 
     def run(self):
         c = Counter()
-        fp_counts = self.output().open('w')
         for tweet_str in self.input().open('r'):
             tweet = json.loads(tweet_str)
             c.update([m['screen_name'].lower()
                      for m in tweet['entities']['user_mentions']])
-        json.dump(c, fp_counts, sort_keys=True, indent=2)
-        fp_counts.close()
+        with self.output().open('w') as fp_counts:
+            writer = csv.DictWriter(fp_counts, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['screen_name', 'count'])
+            writer.writeheader()
+            for screen_name, count in c.items():
+                writer.writerow({'screen_name': screen_name,
+                                 'count': count})
+
+
+class EdgelistMentions(luigi.Task):
+    name = luigi.Parameter()
+    term = luigi.Parameter()
+
+    def requires(self):
+        return FetchTweets(name=self.name, term=self.term)
+
+    def output(self):
+        fname = self.input().fn.replace('tweets.json', 'edgelist-mentions.csv')
+        return luigi.LocalTarget(fname)
+
+    def run(self):
+        """Each edge is a tuple containing (screen_name,
+        mentioned_screen_name)"""
+        with self.output().open('w') as fp_csv:
+            writer = csv.DictWriter(fp_csv, delimiter=',',
+                                    fieldnames=('from_user', 'to_user'))
+            writer.writeheader()
+            for tweet_str in self.input().open('r'):
+                tweet = json.loads(tweet_str)
+                for mention in tweet['entities']['user_mentions']:
+                    writer.writerow({'from_user': tweet['user']['screen_name'],
+                                     'to_user': mention['screen_name']})
 
 
 class CountMedia(luigi.Task):
@@ -169,51 +239,59 @@ class CountMedia(luigi.Task):
         return FetchTweets(name=self.name, term=self.term)
 
     def output(self):
-        fname = self.input().fn.replace('.json', '-media.json')
+        fname = self.input().fn.replace('tweets.json', 'count-media.csv')
         return luigi.LocalTarget(fname)
 
     def run(self):
         c = Counter()
-        fp_counts = self.output().open('w')
         for tweet_str in self.input().open('r'):
             tweet = json.loads(tweet_str)
             c.update([m['media_url']
                      for m in tweet['entities'].get('media', [])
                      if m['type'] == 'photo'])
-        json.dump(c, fp_counts, sort_keys=True, indent=2)
-        fp_counts.close()
+        with self.output().open('w') as fp_counts:
+            writer = csv.DictWriter(fp_counts, delimiter=',',
+                                    quoting=csv.QUOTE_MINIMAL,
+                                    fieldnames=['url', 'count'])
+            writer.writeheader()
+            for url, count in c.items():
+                writer.writerow({'url': url, 'count': count})
 
 
 class FetchMedia(luigi.Task):
     name = luigi.Parameter()
     term = luigi.Parameter()
-    media_files = []
+    is_complete = False
 
     def requires(self):
         return CountMedia(name=self.name, term=self.term)
 
     def run(self):
-        dirname = 'data/%s-media' % self.name
+        dirname = 'data/%s/media' % self.name
         os.makedirs(dirname, exist_ok=True)
         # lots of hits to same server, so let requests pool connections
         session = requests.Session()
-        media = json.load(self.input().open('r'))
-
-        for url, count in media.items():
-            parsed_url = urlparse(url)
-            fname = parsed_url.path.split('/')[-1]
-            if len(fname) == 0:
-                continue
-            self.media_files.append('%s/%s' % (dirname, fname))
-            r = session.get(url)
-            if r.ok:
-                with open('%s/%s' % (dirname, fname), 'wb') as media_file:
-                    media_file.write(r.content)
+        with self.input().open('r') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                parsed_url = urlparse(row['url'])
+                fname = parsed_url.path.split('/')[-1]
+                if len(fname) == 0:
+                    continue
+                media_files.append('%s/%s' % (dirname, fname))
+                r = session.get(row['url'])
+                if r.ok:
+                    with open('%s/%s' % (dirname, fname), 'wb') as media_file:
+                        media_file.write(r.content)
+            self.is_complete = True
 
     def output(self):
         # ensure only one successful fetch for each url
         # unless FetchTweets is called again with a new hash
-        return [luigi.LocalTarget(fname) for fname in self.media_files]
+        return [luigi.LocalTarget(fname) for fname in media_files]
+
+    def complete(self):
+        return self.is_complete
 
 
 class RunFlow(luigi.Task):
@@ -224,8 +302,9 @@ class RunFlow(luigi.Task):
 
     def requires(self):
         return CountHashtags(name=self.name, term=self.term), \
+            EdgelistHashtags(name=self.name, term=self.term), \
             CountUrls(name=self.name, term=self.term), \
             CountDomains(name=self.name, term=self.term), \
             CountMentions(name=self.name, term=self.term), \
-            CountMedia(name=self.name, term=self.term), \
+            EdgelistMentions(name=self.name, term=self.term), \
             FetchMedia(name=self.name, term=self.term)
