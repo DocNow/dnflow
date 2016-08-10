ad511d2a92737eeb9f174250f4b43fdad25f7f8e
Ed Summers <ehs@pobox.com>
2016-08-10 18:49:48 -0400

    correct config var

--- ui.py
+++ ui.py
@@ -14,8 +14,13 @@ from queue_tasks import run_flow
 app = Flask(__name__)
 app.config.from_pyfile('dnflow.cfg')
 
-redis_conn = redis.StrictRedis(host=app.config['REDIS'], charset='utf-8',
-                               decode_responses=True)
+redis_conn = redis.StrictRedis(
+    host=app.config['REDIS_HOST'],
+    port=app.config['REDIS_PORT'],
+    db=app.config['REDIS_DB'],
+    charset='utf-8',
+    decode_responses=True
+)
 q = Queue(connection=redis_conn)
 
 logging.getLogger().setLevel(logging.DEBUG)
