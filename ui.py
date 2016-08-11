909536d0bb96d2553b7a4e7fa10830a2ff9dfad1
Ed Summers <ehs@pobox.com>
2016-08-10 20:06:12 -0400

    DB seems to need to default

--- ui.py
+++ ui.py
@@ -17,7 +17,6 @@ app.config.from_pyfile('dnflow.cfg')
 redis_conn = redis.StrictRedis(
     host=app.config['REDIS_HOST'],
     port=app.config['REDIS_PORT'],
-    db=app.config['REDIS_DB'],
     charset='utf-8',
     decode_responses=True
 )
